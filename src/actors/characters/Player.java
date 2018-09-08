e59aebe9fc4e5ca5a5462a171c2e0bbb9243820a
Cole <3ef4c1bb@opayq.com>
2018-09-07 20:03:43 -0500

    Fix GPL notices, update Mayflower

--- src/actors/characters/Player.java
+++ src/actors/characters/Player.java
@@ -15,27 +15,9 @@
  * along with mayrio.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-/*
- * This file is part of mayrio.
- *
- * mayrio is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * mayrio is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with mayrio.  If not, see <https://www.gnu.org/licenses/>.
- */
-
 package actors.characters;
 
 import actors.core.AnimatedActor;
-import mayflower.Actor;
 
 public class Player extends AnimatedActor {
 }
--- src/actors/core/Animation.java
+++ src/actors/core/Animation.java
@@ -15,24 +15,8 @@
  * along with mayrio.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-/*
- * This file is part of mayrio.
- *
- * mayrio is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * mayrio is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with mayrio.  If not, see <https://www.gnu.org/licenses/>.
- */
-
 package actors.core;
+
 import mayflower.MayflowerImage;
 
 public class Animation {
@@ -41,12 +25,10 @@ public class Animation {
     private int currentFrame;
     private int frameRate;
 
-    public Animation(int rate, String[] filenames)
-    {
+    public Animation(int rate, String[] filenames) {
         frames = new MayflowerImage[filenames.length];
 
-        for(int i=0; i < filenames.length; i++)
-        {
+        for (int i = 0; i < filenames.length; i++) {
             frames[i] = new MayflowerImage(filenames[i]);
         }
 
@@ -54,31 +36,25 @@ public class Animation {
         frameRate = rate;
     }
 
-    public int getFrameRate()
-    {
+    public int getFrameRate() {
         return frameRate;
     }
 
-    public MayflowerImage getNextFrame()
-    {
+    public MayflowerImage getNextFrame() {
         currentFrame = currentFrame % frames.length;
         MayflowerImage ret = frames[currentFrame];
         currentFrame = currentFrame + 1;
         return ret;
     }
 
-    public void resize(int w, int h)
-    {
-        for(MayflowerImage p : frames)
-        {
+    public void resize(int w, int h) {
+        for (MayflowerImage p : frames) {
             p.scale(w, h);
         }
     }
 
-    public void setTransparency(int percent)
-    {
-        for(MayflowerImage p : frames)
-        {
+    public void setTransparency(int percent) {
+        for (MayflowerImage p : frames) {
             p.setTransparency(percent);
         }
     }
